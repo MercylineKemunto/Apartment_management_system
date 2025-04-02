ce471428d3a4419788b439d8614fdd29510e79dc
MercylineKemunto <mercylinekemunto580@gmail.com>
2025-04-02 13:13:16 +0300

    added necessary requirements to my seetings.py and templates

--- rental_management/rental_management/settings.py
+++ rental_management/rental_management/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
 
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
+    
     'django_filters',
     'api',
 ]
